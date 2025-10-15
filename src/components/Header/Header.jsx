220486c53e2e99ba34ecb3fb6b9df39776a7ee54
TamaraHeater <tamaraheater@gmail.com>
2025-10-15 17:04:22 -0500

    update images in asseets

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,15 @@
 function Header() {
-  return <header>WTWR</header>;
+  return (
+    <header className="header">
+      <img src="" alt="" className="header__logo" />
+      <p className="header__date-location">MONTH DATE, LOCATION</p>
+      <button className="header__add-clothes-button">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Name:First Last</p>
+        <img src="" alt="" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
