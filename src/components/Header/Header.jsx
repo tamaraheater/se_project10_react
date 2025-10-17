66e5a49fb43fef36d62e24f34bbc3f7234c550d4
TamaraHeater <tamaraheater@gmail.com>
2025-10-17 14:38:50 -0500

    installed fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import "./Header.css";
-import logo from "../../src/assets/logo.svg";
-import avatar from "../../src/assets/ellipse-18.svg";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
 
 function Header() {
   return (
     <header className="header">
       <img src={logo} alt="Logo" className="header__logo" />
-      <p className="header__date-location">MONTH DATE, LOCATION</p>
+      <p className="header__date-location">DATE, LOCATION</p>
       <button className="header__add-clothes-button">+ Add Clothes</button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
