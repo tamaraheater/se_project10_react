b53bc539189bc29d651c8daa862247e60e004bcb
TamaraHeater <tamaraheater@gmail.com>
2025-10-18 01:55:16 -0500

    errors corrected, still an issue with having to commit in two batches

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,12 +4,12 @@ function Main() {
   return (
     <main>
       <WeatherCard />
-      <secton className="cards">
+      <section className="cards">
         <h2 className="cards__title">
           Today is 75 &deg; F / You may want to wear:
         </h2>
         {/*cards*/}
-      </secton>
+      </section>
     </main>
   );
 }
