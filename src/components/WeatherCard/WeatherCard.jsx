62964eced017840a491ad258a05220f1c42d7de0
TamaraHeater <tamaraheater@gmail.com>
2025-10-18 00:15:08 -0500

    only partial transmit of commits, why?

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,11 @@
 import "./WeatherCard.css";
+import sunny from "../../assets/sunny.svg";
 
 function WeatherCard() {
   return (
     <section className="weather-card">
       <p className="weather-card__temp">75 &deg; F</p>
-      <img src="" alt="" className="weather-card__image" />
+      <img src={sunny} alt="sunny" className="weather-card__image" />
     </section>
   );
 }
