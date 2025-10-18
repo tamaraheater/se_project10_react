247ebe68b0698c35cd8919e74c0437b4bc9ed4a6
TamaraHeater <tamaraheater@gmail.com>
2025-10-18 00:12:47 -0500

    added weatercard and rough code for jsx and css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,17 @@
-const Main = () => {
-  // JS code
+import WeatherCard from "../WeatherCard/WeatherCard";
 
-  return <div></div>;
-};
+function Main() {
+  return (
+    <main>
+      <WeatherCard />
+      <secton className="cards">
+        <h2 className="cards__title">
+          Today is 75 &deg; F / You may want to wear:
+        </h2>
+        {/*cards*/}
+      </secton>
+    </main>
+  );
+}
 
 export default Main;
---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,12 @@
+import "./WeatherCard.css";
+
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">75 &deg; F</p>
+      <img src="" alt="" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
