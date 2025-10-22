a342a892b9f62368f6a21561d5e7bb13c4b3f63d
TamaraHeater <tamaraheater@gmail.com>
2025-10-22 07:37:37 -0500

    itemcards styling

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,10 +1,12 @@
+import "./ItemCard.css";
+
 function ItemCard({ item }) {
-  /*Props*/
+  /*Props needs further study*/
   return (
-    <div>
-      <h2>{item.name}</h2>
-      <img src={item.link} alt={item.name} />
-    </div>
+    <li className="item-card">
+      <h2 className="item-card__name">{item.name}</h2>
+      <img className="item-card__image" src={item.link} alt={item.name} />
+    </li>
   );
 }
 
