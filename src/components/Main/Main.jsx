47810a802b9a0fe94464f8c9d9cc9b9ae2c2fb23
TamaraHeater <tamaraheater@gmail.com>
2025-10-22 09:30:49 -0500

    correct style errors

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,9 +13,9 @@ function Main({ weatherData }) {
         </h2>
         <ul className="cards__list">
           {defaultClothingItems
-            // .filter((item) => {
-            //   return item.weather === weatherData.type;
-            // })
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
             .map((item) => {
               return <ItemCard key={item._id} item={item} />; /*Props*/
             })}
