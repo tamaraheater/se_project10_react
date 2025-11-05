d11e3d2b45ab9d62c7765105983848da3ef89194
TamaraHeater <tamaraheater@gmail.com>
2025-11-05 09:52:49 -0600

    updates to json and pkgjson and viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/se_project10_react/",
   plugins: [react()],
   server: {
     port: 3000,
