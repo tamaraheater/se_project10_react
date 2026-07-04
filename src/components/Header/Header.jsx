109ebc5297693c0b228b2cfeb20997cf8a63654a
TamaraHeater <tamaraheater@gmail.com>
2026-07-03 20:46:29 -0500

    update header again and user context

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";                     
-import { CurrentUserContext } from "../../contexts/CurrentUserContext.jsx";   
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";   
 
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import "./Header.css";
